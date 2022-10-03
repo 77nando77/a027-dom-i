d5445362f64f4736c922568210b295bc2961c08f
luizfk <luiz.kessler7@gmail.com>
2022-10-03 13:30:15 -0300

    dom1

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 const frutas = ["laranja", "limão", "uva"];
+
+const adicionar = () =>{
+    const fruta1 = document.getElementById("fruta-1")
+    fruta1.innerHTML = frutas[0]
+    const fruta2 = document.getElementById("fruta-2")
+    fruta2.innerHTML = frutas[1]
+    const fruta3 = document.getElementById("fruta-3")
+    fruta3.innerHTML = frutas[2]
+    let nome = document.getElementById("nome").value
+    let fruta4 = document.getElementById("fruta-4")
+    fruta4.innerHTML = nome
+    nome = document.getElementById("nome")
+    nome.value = ""
+}  
+
+const novaFruta = document.getElementById("nome")
+console.log(novaFruta.name)
